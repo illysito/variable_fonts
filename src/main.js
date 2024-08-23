8465b8c1c7d5d2f770b84fd5a7f7fad5952c9317
illysito <illyalukyanovdesign@gmail.com>
2024-08-23 14:35:04 +0100

    sdfhgj

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
+// import { gsap } from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
+console.log('VamooooooO!')
